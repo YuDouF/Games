428c5c5967b53ff82cf6eaf2d136aa23346f94bf
hellocp2014 <453342404@qq.com>
2017-04-21 21:18:07 +0800

    delete All

--- RetroSnake/TargetSquare.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Square.h"
-#include "common.h"
-class TargetSquare : public Square{
-public:
-	TargetSquare(int width, int height);
-	void CreateTargetSquare(int leftX, int rightX, int topY, int bottomY);
-	void Display();
-};
--- Tetris/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Component.h"
-#include "LUpShape.h"
-int main(){
-	Component* lUpShape = new LUpShape(10, 10);
-	lUpShape->Display();
-	lUpShape->ChangeShapeDirection(lUpShape);
-	lUpShape->Display();
-
-}
